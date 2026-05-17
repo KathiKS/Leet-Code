a3f85d1635909994c4c001ce70a43977f2bcf466
Karthikeyan Senthilkumar <154965889+KathiKS@users.noreply.github.com>
2026-05-17 13:04:10 +0530

    Time: 4 ms (99.99%), Space: 47.1 MB (28.98%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,45 @@
+class MinStack {
+    ListNode head;
+    int min;
+    public MinStack() {
+        head=null;
+        min=Integer.MAX_VALUE;
+    }
+    public void push(int val) {
+        if(min>val){
+            min=val;
+        }
+        head=new ListNode(val,min,head);
+    }
+    public void pop() {
+        head=head.next;
+        if(head==null){
+            min=Integer.MAX_VALUE;
+        }else{
+            min=head.currentMin;
+        }
+    }
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.currentMin;
+    }
+}
+class ListNode{
+    int val;
+    int currentMin;
+    ListNode next;
+    ListNode(){
+
+    }
+    ListNode(int val){
+        this.val=val;
+    }
+    ListNode(int val,int currMin,ListNode next){
+        this.val=val;
+        this.currentMin=currMin;
+        this.next=next;
+    }
+}
